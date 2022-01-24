9f3259f685e877be9a3f7c6200ec5bfb3bc57c47
GlebSharipov <glebsharipov@gmail.com>
2022-01-24 14:51:30 +0600

    fix bug

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -3,17 +3,21 @@ import styles from "./Slider.module.scss";
 
 function Slider() {
   return (
-    <div className={styles.slider}>
-      <div className={styles.slide}>
-        <div className={styles.slideLeft}>
-          <img className={styles.logo} src="/img/logoslide.png" alt="Logo" />
-          <h2>Stan Smith, Forever!</h2>
-          <button>Купить</button>
-        </div>
+    <div className={styles.slide}>
+      <div className={styles.slideLeft}>
+        <img
+          className={styles.logo}
+          height={40}
+          width={99}
+          src="/img/logoslide.png"
+          alt="Logo"
+        />
+        <h2>Stan Smith, Forever!</h2>
+        <button>Купить</button>
+      </div>
 
-        <div>
-          <img src="/img/slide1.png" alt="Slide" />
-        </div>
+      <div className="imgSlide">
+        <img src="/img/slide1.png" alt="Slide" />
       </div>
     </div>
   );
